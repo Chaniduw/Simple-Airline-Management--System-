873e5507715c0974091b40aaac49a59a4c753fe7
Chaniduw <106092819+Chaniduw@users.noreply.github.com>
2023-12-17 01:42:56 +0530

    Add files via upload

---
+++ AirlineSys2/src/main/java/com/mycompany/airlinesys2/AirlineSys2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airlinesys2;
+
+/**
+ *
+ * @author User
+ */
+public class AirlineSys2 {
+
+    public static void main(String[] args) {
+      
+        Login l1=new Login();
+        l1.setVisible(true);
+        
+       // Splash s1=new Splash();
+      // s1.setVisible(true);
+    }
+}
